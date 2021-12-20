b2229cf21f0ef0be50be8ddcbeef04fc584fb82d
unknown <harshithxworkz@gmail.com>
2021-12-20 22:39:57 +0530

    example of learning 6

---
+++ ROYALCHAR.java
@@ -0,0 +1,18 @@
+class ROYALCHAR{
+               public static void main(String  []args){
+                 char   []harshi  = {'H','A','R','S','H','I','T','H','S','R',  };
+                  System.out.println(harshi[0]);
+                  System.out.println(harshi[1]);
+                  System.out.println(harshi[2]);
+                  System.out.println(harshi[3]);
+                  System.out.println(harshi[4]);
+                  System.out.println(harshi[5]);
+                   System.out.println(harshi[6]);
+                  System.out.println(harshi[7]);
+                  System.out.println(harshi[8]);
+                  System.out.println(harshi[9]);
+               
+             
+          }
+    }     
+          
